33d646038f3eb83a0408075548f647aa5f30fa39
Dimitris Xenakis <d.xenakis@ieee.org>
2026-07-13 21:33:32 +0200

    fix(Testing): Validate reused database migrations
    Issue: #8648

--- tests/ruciopytest/infra_manager.py
+++ tests/ruciopytest/infra_manager.py
@@ -69,7 +69,7 @@ class InfraManager:
         second_vo = self.case.suite == "multi_vo" and multi_vo_leg == "ts2"
         if not database_ready and not second_vo:
             self._reset_database()
-        if not database_ready and not second_vo:
+        if not second_vo:
             self._run_alembic_migration()
 
         self._restart_httpd()
